222454b3afe67073adac3cb4a88672e56c3b5a3e
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 22:14:06 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,6 +8,8 @@ def my_all?(collection)
     i = i + 1   
   end
   if block_return_values.include?(false)
+  # include? makes this .collect into .all? 
+  # the block outputs a true or false depending on its conditions and the if statement returns false or true "echoing" the block
     false
   else
     true
