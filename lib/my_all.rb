644a68bdcf4d01df45b6578ddd7d1c5cec563b2e
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-15 14:37:15 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,4 +1,28 @@
 require 'pry'
+=begin
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+<<<<<<< HEAD
+=======
+  binding.pry
+>>>>>>> c477acfdf840f59d548e0d4f112ce4fbd53daaa5
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+end
+
+<<<<<<< HEAD
+my_all?([1,2,3]) {|i| i < 3}
+=======
+>>>>>>> c477acfdf840f59d548e0d4f112ce4fbd53daaa5
+=end
 
 def my_all?(collection)
   i = 0
@@ -7,11 +31,6 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-  if block_return_values.include?(false)
-    false
-  else
-    true
-  end
+  
+  block_return_values.include?(false) ? false : true
 end
-
-my_all?([1,2,3]) {|i| i < 3}
