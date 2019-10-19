b628c482bb59ae213b3b47601bbd2d0a23c823f5
lynchrFI <lynchrbusiness@gmail.com>
2019-10-19 18:11:16 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
-require 'pry'
 
-def my_all?(collection)
-
-end
+all_odd = [1,3].all? do |number|
+    number.odd? # Will evaluate to true for 1, true for 3
+  end #=> true
+  all_odd #=> true
+  
+  [4, 8, 9, 984].any?{|i| i < 9842}
+  
+  [1,2,3].detect{|i| i.odd?} #=> 1
+end 
