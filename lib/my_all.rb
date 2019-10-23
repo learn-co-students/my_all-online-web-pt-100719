c18beeb749753bf67c9c8f1c4d5ec5b718d2b060
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-10-23 19:05:16 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,4 +2,20 @@ require 'pry'
 
 def my_all?(collection)
 
+  block_return_values = [] # Setting up an empty array to handle an empty collection
+  i = 0 # Declares our counter variable
+
+  while i < collection.length
+
+    block_return_values << yield(collection[i])
+    i = i + 1
+
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+
 end
