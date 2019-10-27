185fe756159e5d5a2d661202b81a0ded0198876a
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-27 17:38:41 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+  b = 0
+  the_choosen = []
+  while b < collection.length 
+    the_choosen << yield(collection[b])
+    b += 1 
+  end
+  
+    if the_choosen.include?(false)
+    false
+  else
+    true
+  end
 end
